19eccb5c5d37c99c38391de825fe7161007e380c
michealjwellis <mellis.eriecon@gmail.com>
2022-05-04 21:43:12 -0400

    deployed site correctly :D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ const iconMenu = [
     {
         listIcon: <Home />,
         listText: "Home",
-        listPath: "/"
+        listPath: "/NewPortfolio"
     },
     {
         listIcon: <AssignmentInd />,
--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ render(
               <h1>404</h1>
               <h2>PAGE NOT FOUND</h2>
               <h3>BACK TO HOME?</h3>
-              <a href="/" class="yes">YES</a>
+              <a href="/NewPortfolio" class="yes">YES</a>
               <br></br>
               <a href="https://www.youtube.com/watch?v=G3AfIvJBcGo">NO</a>
             </div>
